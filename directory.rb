777c003d200ca9a37f0d298ebf9651755f71ded9
Hadi <hadichalabi@gmail.com>
2014-09-09 22:06:30 +0100

    Added Load List Method

--- directory.rb
+++ directory.rb
@@ -35,6 +35,7 @@ def print_menu
 		puts "1. Input the students"
 		puts "2. Show the students"
 		puts "3. Save the list to students.csv"
+		puts "4. Load the list from students.csv"
 		puts "9. Exit" #9. because we'll be adding more items
 end
 
@@ -42,29 +43,6 @@ def show_students
 	print_header
 	print_students_list
 	print_footer
-end
-
-def process(selection)
-	case selection
-		#3. do what the user has asked
-		when "1"
-			input_students
-		when "2"
-			show_students
-		when "3"
-			save_students
-		when "9"
-			exit #this will cause the program to terminate
-		else
-			puts "I don't know what you meant, try again"
-	end
-end
-
-def interactive_menu
-	loop do 
-		print_menu
-		process(gets.chomp)
-  end
 end
 
 def save_students
@@ -77,6 +55,40 @@ def save_students
 	file.puts csv_line
 	end
 	file.close
+end
+
+def load_students
+	file = File.open("students.csv", "r")
+	file.readlines.each do |line|
+	name, cohort = line.chomp.split(',')
+	  @students << {:name => name, :cohort => cohort.to_sym}
+	end
+	file.close
+end
+
+def process(selection)
+	case selection
+		#3. do what the user has asked
+		when "1"
+			input_students
+		when "2"
+			show_students
+		when "3"
+			save_students
+		when "4"
+			load_students
+		when "9"
+			exit #this will cause the program to terminate
+		else
+			puts "I don't know what you meant, try again"
+	end
+end
+
+def interactive_menu
+	loop do 
+		print_menu
+		process(gets.chomp)
+  end
 end
 
 
